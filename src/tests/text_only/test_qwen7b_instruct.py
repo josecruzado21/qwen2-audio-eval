62ceb4b48535ff2a25ca7f9a2ce024195f80170c
Jose Cruzado <josecruzado2103@gmail.com>
2025-05-16 16:40:49 -0500

    feat: change prompt

--- src/tests/text_only/test_qwen7b_instruct.py
+++ src/tests/text_only/test_qwen7b_instruct.py
@@ -9,11 +9,15 @@ processor = AutoProcessor.from_pretrained("Qwen/Qwen2-Audio-7B-Instruct",
 model = Qwen2AudioForConditionalGeneration.from_pretrained("Qwen/Qwen2-Audio-7B-Instruct", device_map="auto", 
                                                            cache_dir = "/share/data/lang/users/ttic_31110/jcruzado/models/")
 
-conversation = [
-    {'role': 'system', 'content': 'You are a helpful assistant.'}, 
-    {"role": "user", "content": [
-        {"type": "text", "text": "What is the capital of france?"},
-    ]}]
+# conversation = [
+#     {'role': 'system', 'content': 'You are a helpful assistant.'}, 
+#     {"role": "user", "content": [
+#         {"type": "text", "text": "What is the capital of france?"},
+#     ]}]
+conversation = [{'role': 'system', 'content': 'You are a helpful assistant. Answer the question with the correct letter'}, 
+                {'role': 'user', 'content': 
+                 [{'type': 'text', 'text': 'Question: If a psychologist acts as both a fact witness for the plaintiff and an expert witness for the court in a criminal trial, she has acted:\nA. unethically by accepting dual roles.\nB. ethically as long as she did not have a prior relationship with the plaintiff.\nC. ethically as long as she clarifies her roles with all parties.\nD. ethically as long as she obtains a waiver from the court.\nAnswer:'}
+                  ]}]
 text = processor.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
 inputs = processor(text=text, return_tensors="pt", padding=True)
 # inputs.input_ids = inputs.input_ids.to("cuda")
