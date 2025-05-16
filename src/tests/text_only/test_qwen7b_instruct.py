e67a20223ff11f25d6b9e02ff0eb1f023a2dadf6
Jose Cruzado <josecruzado2103@gmail.com>
2025-05-16 11:31:14 -0500

    feat: adding to cude

--- src/tests/text_only/test_qwen7b_instruct.py
+++ src/tests/text_only/test_qwen7b_instruct.py
@@ -15,7 +15,8 @@ conversation = [
     ]}]
 text = processor.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
 inputs = processor(text=text, return_tensors="pt", padding=True)
-inputs.input_ids = inputs.input_ids.to("cuda")
+# inputs.input_ids = inputs.input_ids.to("cuda")
+inputs = inputs.to("cuda")
 generate_ids = model.generate(**inputs, max_length=256)
 generate_ids = generate_ids[:, inputs.input_ids.size(1):]
 response = processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
