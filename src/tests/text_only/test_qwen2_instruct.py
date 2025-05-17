d6767f2ad4c517692c4cef5d2dd5fbea2a63bc85
Jose Cruzado <josecruzado2103@gmail.com>
2025-05-17 12:04:46 -0500

    feat: add new test to QwenLM

--- src/tests/text_only/test_qwen2_instruct.py
+++ src/tests/text_only/test_qwen2_instruct.py
@@ -10,5 +10,11 @@ model = AutoModelForCausalLM.from_pretrained(
     trust_remote_code=True,
     cache_dir = "/share/data/lang/users/ttic_31110/jcruzado/models/"
 )
-response, history = model.chat(tokenizer, "hello", history=None)
+response, history = model.chat(tokenizer,"If a psychologist acts as both a fact witness for the plaintiff and an expert witness for the court in a criminal trial, she has acted:\n"
+    "A. unethically by accepting dual roles.\n"
+    "B. ethically as long as she did not have a prior relationship with the plaintiff.\n"
+    "C. ethically as long as she clarifies her roles with all parties.\n"
+    "D. ethically as long as she obtains a waiver from the court.\n"
+    "Answer (A, B, C, or D only):", history=None, system = "Give ONLY the letter of the correct answer (A, B, C, or D). Your answer should be one character long A, B, C, or D")
+print(history)
 print(response)
