80f7dddbb4c6d7917224a23196d86054c0f3b556
Jose Cruzado <josecruzado2103@gmail.com>
2025-05-17 12:12:20 -0500

    feat: add new test to QwenLM

--- src/tests/text_only/test_qwen2_instruct.py
+++ src/tests/text_only/test_qwen2_instruct.py
@@ -18,8 +18,12 @@ response, history = model.chat(tokenizer,"If a psychologist acts as both a fact 
     "B. ethically as long as she did not have a prior relationship with the plaintiff.\n"
     "C. ethically as long as she clarifies her roles with all parties.\n"
     "D. ethically as long as she obtains a waiver from the court.\n"
-    "Answer (A, B, C, or D only):", history=None, system = "Give ONLY the letter of the correct answer (A, B, C, or D). Your answer should be one character long A, B, C, or D")
+    "Answer (A, B, C, or D only):", 
+    history=None, 
+    system = "Give ONLY the letter of the correct answer (A, B, C, or D). Your answer should be one character long A, B, C, or D",
+    max_new_tokens=8)
 end = time.time()
 print("Time to generate:", end - start)
-print(history)
-print(response)
+print("History", history)
+print()
+print("Response:", response)
