af543f8ca12f2571c5e057d1a52e28f18855a20e
Jose Cruzado <josecruzado2103@gmail.com>
2025-05-16 22:15:46 -0500

    feat: add breakpoint

--- src/benchmarking/qwen2audio_textonly.py
+++ src/benchmarking/qwen2audio_textonly.py
@@ -32,6 +32,7 @@ def qwen2audio_textonly_inference(MMLU_data):
     model = Qwen2AudioForConditionalGeneration.from_pretrained("Qwen/Qwen2-Audio-7B-Instruct", device_map="auto", 
                                                             cache_dir = "/share/data/lang/users/ttic_31110/jcruzado/models/")
     for idx, row in tqdm(MMLU_data.iterrows()):
+        breakpoint()
         if row["qwen2audio_textonly_response"] == "":
             conversation = qwen2audio_textonly_chat_prompt(row)
             text = processor.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
